f7fb62801d1bbef10809e107105283dff791ae0d
Aron <aronnaylor@protonmail.com>
2020-12-05 11:43:02 +0000

    fix bug on africa page, fix deploy error

--- src/pages/blog/africa.js
+++ src/pages/blog/africa.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link, graphql, useStaticQuery } from "gatsby"
+import { Row, Col, Card, Badge } from "react-bootstrap"
 
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
